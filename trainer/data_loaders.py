9d22f04655ee5aa11757934bb9dccb9aaccb5fc8
ThomasLee <baizhen9406@163.com>
2023-04-05 22:21:36 +0800

    delete verbose codes in dataloader

--- trainer/data_loaders.py
+++ trainer/data_loaders.py
@@ -114,20 +114,6 @@ class MultiWOZDataLoader(DataLoader):
         return DataLoader(dataset = self.datasets.vali_data, **self.init_kwargs)
 
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
-
-
 import datasets
 from transformers import AutoTokenizer, AddedToken
 from torch.utils.data.sampler import BatchSampler, RandomSampler, SequentialSampler
