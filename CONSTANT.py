c167763d210e87064fa0873e876ff3a4ba8f0ae4
Thomas-Lee <baizhen9406@163.com>
2023-04-06 10:00:18 +0800

    Delete CONSTANT.py
    d

--- CONSTANT.py
+++
@@ -1,8 +0,0 @@
-import os
-CUR_DIR = os.path.dirname(os.path.abspath(__file__))
-ONTO_DIR = os.path.join(CUR_DIR, "data/dstc2/ontology_dstc2.json")
-TRAIN_DEV_DIR=os.path.join(CUR_DIR, "data/dstc2/traindev")
-TEST_DIR=os.path.join(CUR_DIR, "data/dstc2/test")
-# TRAIN_FILE_LIST_DIR=os.path.join(TRAIN_DEV_DIR,'dstc2_train.flist')
-# DEV_FILE_LIST_DIR=os.path.join(TRAIN_DEV_DIR,'dstc2_dev.flist')
-# TEST_FILE_LIST_DIR=os.path.join(TEST_DIR, 'dstc2_test.flist')
